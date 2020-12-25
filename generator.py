74407986d0385944d7e361391874937f5df89f03
khanhhhh <nguyenngockhanh.pbc@gmail.com>
2020-12-25 16:49:24 +0800

    fixed generator

--- generator.py
+++ generator.py
@@ -5,6 +5,7 @@ import numpy as np
 from solver import solve_all
 
 Pos = tuple[int, int, int]
+Cell = tuple[int, int]
 
 def get_num_solutions(board: np.ndarray, cap: int = 2) -> int:
     num_solutions = 0
@@ -24,9 +25,19 @@ def get_position() -> Iterator[Pos]:
     np.random.shuffle(position_list)
     return iter(position_list)
 
+def get_cell() -> Iterator[Cell]:
+    cell_list = []
+    for y in range(9):
+        for x in range(9):
+            cell = y, x
+            cell_list.append(cell)
+    np.random.shuffle(cell_list)
+    return iter(cell_list)
+
 def generate(seed: int) -> np.ndarray:
     np.random.seed(seed)
     board = np.zeros((9, 9), dtype=int) - 1
+    # generate unique solution board
     for pos in get_position():
         y, x, v = pos
         if 0 <= board[y, x] and board[y, x] < 9:
@@ -34,6 +45,22 @@ def generate(seed: int) -> np.ndarray:
         board[y, x] = v
         num_solutions = get_num_solutions(board)
         if num_solutions == 1:
-            return board
+            break
         if num_solutions == 0:
             board[y, x] = -1
+    # simplify board
+    for cell in get_cell():
+        y, x = cell
+        if not (0 <= board[y, x] and board[y, x] < 9):
+            continue
+        v = board[y, x]
+        board[y, x] = -1
+        num_solutions = get_num_solutions(board)
+        if num_solutions == 0:
+            raise RuntimeError("something wrong")
+        if num_solutions == 2:
+            board[y, x] = v
+
+    return board
+
+
