a507357fbd35df7616eb2aaf4c21d37ed6340b7e
khanhhhh <nguyenngockhanh.pbc@gmail.com>
2021-01-09 21:07:49 +0800

    fixed

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ import (
 func main() {
 	seed := int(time.Now().UnixNano())
 	portno := 3000
+	addr := fmt.Sprintf("0.0.0.0:%d", portno)
 	s := gui.NewServer(seed)
-	fmt.Printf("Server is up at: http://0.0.0.0:%d\n", portno)
-	s.Run(fmt.Sprintf(":%d", portno))
+	fmt.Printf("Server is up at: http://%s \n", addr)
+	s.Run(addr)
 	return
 }
